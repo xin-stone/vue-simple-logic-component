f428f075001675e59a8fc53d1a2a987b4d698852
songweian <songweian@vipkid.com.cn>
2020-05-14 01:40:45 +0800

    add SimpleLogic.vue

--- src/components/SimpleLogic.vue
+++ src/components/SimpleLogic.vue
@@ -1,12 +1,36 @@
 <template>
     <div>
-        hello simple logic
+        {{this.renderNode(value)}}
     </div>
 </template>
 
 <script>
     export default {
-        name: "SimpleLogic"
+        name: "SimpleLogic",
+        props: {
+            value: {
+                nodeType: String,
+                logicType: String,
+                values: []
+            }
+        },
+        watch: {
+            'value': function (o, n) {
+                this.value = n;
+
+            }
+        },
+        methods: {
+            renderNode() {
+
+            },
+            renderGroupNode() {
+
+            },
+            renderRuleNode: function () {
+                return `<div> hello world render rule node</div>`;
+            }
+        }
     }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="home">
-    <SimpleLogic />
-  </div>
+    <div class="home">
+        <div>
+            rootNodeType:{{this.logicData.logicType}}<br/>
+            logicType:{{this.logicData.logicType}}<br/>
+        </div>
+        <br/>
+        ===================================================
+        <br/>
+
+        <SimpleLogic v-model="logicData"/>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import SimpleLogic from "@/components/SimpleLogic.vue";
+    // @ is an alias to /src
+    import SimpleLogic from "@/components/SimpleLogic.vue";
 
-export default {
-  name: "Home",
-  components: {
-    SimpleLogic
-  }
-};
+    export default {
+        name: "Home",
+        components: {
+            SimpleLogic
+        },
+        data: function () {
+            return {
+                logicData: {
+                    nodeType: "groupNode",
+                    logicType: "and",
+                    values: []
+                }
+            }
+        }
+    };
 </script>
